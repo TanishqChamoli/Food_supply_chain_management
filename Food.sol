da8e9e36630c5173fb5f2094297463934dadb7bb
Tanishq Chamoli <tanishq.chamoli@gmail.com>
2020-11-01 23:40:40 +0530

    Added a new function.

--- Food.sol
+++ Food.sol
@@ -75,4 +75,7 @@ contract Food {
         crops.push(x);
         people[_farmer].current_quantity[_cropID]=_total_quantity;
     }
+    function userInfo(address _add,uint256 _cropID)public view returns(uint256){
+        return people[_add].current_quantity[_cropID];
+    }
 }
